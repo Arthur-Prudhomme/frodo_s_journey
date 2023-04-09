92c1874e588787e311e71d249e834f953141bf46
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2023-04-09 10:38:00 +0200

    :sparkles: gsap test & imgs reorg

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const loader = document.querySelector('.loader');
 
 window.addEventListener('load', () => {
-
     loader.classList.add('fondu-out');
-
 })
+
+gsap.to("#rivendell", {
+    scrollTrigger: {
+        trigger: "#rivendell",
+        start: "bottom bottom"
+    },
+    x: 500,
+
+});
